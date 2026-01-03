9744e598463826a74cd0609c1ed17b1c13162282
SilkyKitsune <jerd.12345@gmail.com>
2026-01-03 00:11:07 -0600

    Added VanillaBuffs

---
+++ LifeBoost/VanillaBuffs.cs
@@ -0,0 +1,14 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LifeBoost;
+
+public sealed class VanillaBuffs : GlobalBuff
+{
+    public override void Update(int type, Player player, ref int buffIndex)
+    {
+        if (type == BuffID.Lifeforce && player.TryGetModPlayer(out LifeBoostPlayer modPlayer))
+            modPlayer.lifeMultiplier += 0.2f;
+    }
+}
