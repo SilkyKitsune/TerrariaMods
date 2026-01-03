fa95ecad7d62a7cf251f4bb19eee6fe39230bb6d
SilkyKitsune <jerd.12345@gmail.com>
2026-01-02 23:49:05 -0600

    Removed currentDef/currentDefBoost

--- LifeBoost/LifeBoost.cs
+++ LifeBoost/LifeBoost.cs
@@ -10,16 +10,12 @@ internal sealed class LifeBoostPlayer : ModPlayer
     internal int crystalsUsed = 0;
     internal int fruitsUsed = 0;
 
-    internal int currentDef = 0;
-    internal int currentDefBoost = 0;
-
     public override void LoadData(TagCompound tag)
     {
         crystalsUsed = tag.TryGet(nameof(crystalsUsed), out int crystalValue) ? crystalValue : 0;
         fruitsUsed = tag.TryGet(nameof(fruitsUsed), out int fruitValue) ? fruitValue : 0;
     }
 
-    public override void PostUpdate() => currentDef = Player.statDefense - currentDefBoost;
 
     public override void SaveData(TagCompound tag)
     {
